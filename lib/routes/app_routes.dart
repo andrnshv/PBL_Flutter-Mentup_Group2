544afe86ece44a41b119219e8e2d007ce2172198
Lovie-Tonimba <jechonialovie@gmail.com>
2026-04-23 19:16:18 +0700

    tambahan tampilan page profile dan navigasi fitur-fitur

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -14,4 +14,13 @@ class AppRoutes {
   static const mentorTips = '/mentor_tips';
   static const bookingRequest = '/booking_request';
   static const bookingDetail = '/booking_detail';
+
+  static const mentorProfile = '/mentor_profile';
+  static const editProfile = '/edit_profile';
+  static const editRates = '/edit_rates';
+  static const mySchedule = '/my_schedule';
+  static const historySession = '/history_session';
+  static const transactions = '/transactions';
+  static const teachingForm = '/teaching_form';
+  static const settingsAccount = '/settings_account';
 }
