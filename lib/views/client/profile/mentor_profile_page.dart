f43c03566a7a919c1de0301b4c7b7c3e8e6401c6
aiska28 <aiskacaca8@gmail.com>
2026-05-15 20:52:42 +0700

    revisi jarak jadi domisili

--- lib/views/client/profile/mentor_profile_page.dart
+++ lib/views/client/profile/mentor_profile_page.dart
@@ -277,7 +277,7 @@ class MentorProfilePage extends StatelessWidget {
                 children: [
                   _infoItem("Mentoring Sessions ", "20+ "),
                   _infoItem("Price", "Rp ${mentor.price}"),
-                  _infoItem("Distance", "${mentor.dom} km"),
+                  _infoItem("Residence", "${mentor.dom} "),
                 ],
               ),
             ),
